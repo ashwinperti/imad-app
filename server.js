f1b74063d9e8c1f924e8e35de2f5eb1335c4e201
Ashwin Perti <ashwinperti@abes.ac.in>
2017-08-08 12:53:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/', function (req, res) {
+app.get('/article-one', function (req, res) {
     res.send('Article One requested and will be served here');
 });
 
+app.get('/article-two', function (req, res) {
+    res.send('Article One requested and will be served here');
+});
+
+app.get('/article-three', function (req, res) {
+    res.send('Article One requested and will be served here');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
